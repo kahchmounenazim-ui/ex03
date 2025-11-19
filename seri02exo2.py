c74f7443083c9a84234e374d6b1aa459884a4dd1
NAZIM <kahchmounenazim@gmail.com>
2025-11-19 14:42:39 +0100

    Add files via upload

---
+++ seri02exo2.py
@@ -0,0 +1,9 @@
+color  = input("enter the trafic light color")
+if color == "red":
+    print("stop")
+elif color == "yellow":
+    print("slow down") 
+elif color == "green":
+    print("go")
+else:
+    print("invalid color")           
